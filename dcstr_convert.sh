30740ed658b038dc03a8aa7b28e00d94650a6a62
Abhijeet Kasurde <akasurde@redhat.com>
2016-01-28 16:54:53 +0530

    Updated script
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- dcstr_convert.sh
+++ dcstr_convert.sh
@@ -8,21 +8,30 @@ if [[ x"$1" == x"" ]]; then
     echo "$(basename $0) <domain_name>"
     exit 1
 else
-    domain=$1
+    server=$1
 fi
 
 ORGIFS=$IFS
-IFS="." read -r -a domainarray <<< $domain
+IFS="." read -r -a domainarray <<< $server
 IFS=$ORGIFS
 
 dcstr=""
-for element in "${domainarray[@]}"
+domain=""
+for element in "${domainarray[@]:1}"
 do
     if [[ x"$dcstr" == x"" ]]; then
         dcstr="dc=$element"
     else
         dcstr="$dcstr,dc=$element"
     fi
+
+    if [[ x"$domain" == x"" ]]; then
+        domain=$element
+    else
+        domain="$domain.$element"
+    fi
 done
 
-echo $dcstr
+echo "Server : $server"
+echo "Domain : $domain"
+echo "Dcstr  : $dcstr"
