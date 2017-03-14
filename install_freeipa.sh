1e3daf25a3d9a7e2da85ef51df80275a8d6d2ee2
Abhijeet <akasurde@redhat.com>
2017-03-14 11:44:07 +0530

    Update install_freeipa.sh

--- install_freeipa.sh
+++ install_freeipa.sh
@@ -1,8 +1,4 @@
-hostname_new=`hostname -s`."testrelm.test";echo $hostname_new > /etc/hostname;hostname $hostname_new;ip_add=`ip addr|grep "global"|cut -d " " -f6|cut -d "/" -f1|head -n 1`;echo "$ip_add $hostname_new" >> /etc/hosts;cat /etc/hosts && git clone https://github.com/freeipa/freeipa
-&& cd freeipa && dnf builddep -y --spec freeipa.spec.in && dnf install -y java  jsl rpm-build && dnf -y copr enable @freeipa/freeipa-master && pip install pylint &&
-./makerpms.sh && dnf install dist/rpms/*.rpm && mkdir -p ~/.ipa/alias/ && echo Secret123 > ~/.ipa/alias/.pwd && chcon system_u:object_r:ipa_otpd_exec_t:s0 /usr/libexec/ipa/ipa-otpd
-
-
+hostname_new=`hostname -s`."testrelm.test";echo $hostname_new > /etc/hostname;hostname $hostname_new;ip_add=`ip addr|grep "global"|cut -d " " -f6|cut -d "/" -f1|head -n 1`;echo "$ip_add $hostname_new" >> /etc/hosts;cat /etc/hosts && git clone https://github.com/freeipa/freeipa && cd freeipa && dnf builddep -y --spec freeipa.spec.in && dnf install -y java  jsl rpm-build && dnf -y copr enable @freeipa/freeipa-master && pip install pylint && ./makerpms.sh && dnf install -y dist/rpms/*.rpm && mkdir -p ~/.ipa/alias/ && echo Secret123 > ~/.ipa/alias/.pwd && chcon system_u:object_r:ipa_otpd_exec_t:s0 /usr/libexec/ipa/ipa-otpd
 ipa-server-install --ip-address $(ip addr|grep "global"|cut -d " " -f6|cut -d "/" -f1|head -n 1) -r testrelm.test -p 'Secret123' -a 'Secret123' --setup-dns -U
 echo Secret123 | kinit admin
 python lite-server.py
